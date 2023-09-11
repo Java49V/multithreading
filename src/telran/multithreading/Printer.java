66758421501e1c15b9a5008066e24d3c26e96ce8
dvgorsky <48658126+dvgorsky@users.noreply.github.com>
2023-09-11 21:27:38 +0300

    HW #44 PrintThreads

--- src/telran/multithreading/Printer.java
+++ src/telran/multithreading/Printer.java
@@ -1,34 +1,46 @@
 package telran.multithreading;
 
 public class Printer extends Thread {
-	
-	private char[] symbols;
-	private boolean running = true;
+	private static final long SLEEPING_TIME = 100;
+	int printerNumber;
+	Printer next;
+	private int inLine;
+	static int overall;
+	static int nPartitions;
 
-	public void stopPrinter() {
-		this.running = false;
+	public Printer(int printerNumber) {
+		this.printerNumber = printerNumber;
+		inLine = overall / nPartitions;
 	}
 
-	public Printer(String symbols) {
-		
-		this.symbols = symbols.toCharArray();
+	public void setNext(Printer next) {
+		this.next = next;
 	}
+
+	public static void setOverall(int overall) {
+		Printer.overall = overall;
+	}
+
+	public static void setPartitions(int nPartitions) {
+		Printer.nPartitions = nPartitions;
+
+	}
+
 	@Override
 	public void run() {
-		
-		int index = 0;
-		while(running) {
-			System.out.print(symbols[index]);
+		int count = 0;
+		String line = (" " + printerNumber).repeat(inLine);
+		while (count < nPartitions) {
 			try {
-				sleep(1000);
-			} catch (InterruptedException e) {
+				join();
 
-				index++;
-				if(index == symbols.length) {
-					index = 0;
-				}
+			} catch (InterruptedException e) {
+				System.out.println(line);
+				next.interrupt();
+				count++;
 			}
 		}
+
 	}
-	
+
 }
--- src/telran/multithreading/PrinterControllerAppl.java
+++ src/telran/multithreading/PrinterControllerAppl.java
@@ -1,31 +1,35 @@
 package telran.multithreading;
-
-import java.time.Instant;
-import java.time.temporal.ChronoUnit;
-import java.util.Scanner;
+import java.lang.Thread.State;
 
 public class PrinterControllerAppl {
 
-	public static void main(String[] args)   {
-		//Interview question: how to join itself in the main thread
-		Thread.currentThread().interrupt();
-		try {
-			Thread.currentThread().join();
-		} catch (InterruptedException e) {
-			System.out.println("interrupted");
+	private static final int N_THREADS = 50;
+	private static final int N_NUMBERS = 100;
+	private static final int N_PARTITIONS = 10;
+	
+	
+
+	public static void main(String[] args) {
+		Printer.setPartitions(N_PARTITIONS);
+		Printer.setOverall(N_NUMBERS);
+		Printer[] printers=new Printer[N_THREADS];
+		creatingPrinters(printers);
+		
+		printers[0].interrupt();
+		
+	}
+
+	
+
+	private static void creatingPrinters(Printer[] printers) {
+		printers[0]=new Printer(1);
+		for(int i=1; i<printers.length; i++){
+			printers[i]=new Printer(i+1);
+			printers[i-1].setNext(printers[i]);
+			printers[i-1].start();
 		}
-	   Printer printer = new Printer(".*#$%&");
-	   Scanner scanner = new Scanner(System.in);
-	   printer.start();
-	   while(true) {
-		   String line = scanner.nextLine();
-		   if(line.equals("q")) {
-			   break;
-		   }
-		   printer.interrupt();
-		   
-	   }
-	   
-	   printer.stopPrinter();
-}
+		printers[printers.length-1].setNext(printers[0]);
+		printers[printers.length-1].start();
+	}
+
 }
